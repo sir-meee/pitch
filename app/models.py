6a1639d559179f9f8b45d5a20a7b3baec8f2ddde
sir-meee <sammymbevi@gmail.com>
2018-11-18 20:01:16 +0300

    Adds profile details

--- app/models.py
+++ app/models.py
@@ -13,6 +13,8 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
     comments = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
     upvotes = db.relationship('Upvote', backref = 'user', lazy = 'dynamic')
