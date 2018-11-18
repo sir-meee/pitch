b3aaf620f5a08fa3db417353bd96c2e65d22e16b
sir-meee <sammymbevi@gmail.com>
2018-11-18 20:31:17 +0300

    Adds update profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
-from flask import render_template,abort
+from flask import render_template,abort,redirect,url_for
 from . import main
 from flask_login import login_required
 from ..models import User
+from .forms import PitchForm, CommentForm, UpvoteForm, DownvoteForm, UpdateProfile
+from .. import db
 
 @main.route('/')
 def index():
@@ -45,3 +47,17 @@ def profile(uname):
     if user is None:
         abort(404)
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.profile',uname=user.username))
+    return render_template('profile/update.html',form =form)
