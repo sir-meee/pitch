bde8e5884c7d339c86715a22eb7d14910e7bdad2
sir-meee <sammymbevi@gmail.com>
2018-11-18 19:36:58 +0300

    load user id

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-class User(UserMixin,db.Model):
+from . import login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
