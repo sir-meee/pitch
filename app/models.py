004d8b791ff40d16d55bdfdc28d71a787834774e
sir-meee <sammymbevi@gmail.com>
2018-11-18 18:44:32 +0300

    secure password

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
@@ -6,3 +7,11 @@ class User(db.Model):
     pass_secure = db.Column(db.String(255))
     def __repr__(self):
         return f'User {self.username}'
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)     
