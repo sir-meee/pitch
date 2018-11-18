48f126a796e8e5f600c80b3687929055da8ce099
sir-meee <sammymbevi@gmail.com>
2018-11-18 22:47:06 +0300

    Finalizes features

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,25 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,TextAreaField,RadioField
+from wtforms import SubmitField,TextAreaField
 from wtforms.validators import Required
 from wtforms import ValidationError
+
 
 class PitchForm(FlaskForm):
     content = TextAreaField("What would you like to pitch ?",validators=[Required()])
     submit = SubmitField('Submit')
 
 class CommentForm(FlaskForm):
- description = TextAreaField('Add comment',validators=[Required()])
-submit = SubmitField()
-
-#class Downvote(FlaskForm):
-#   submit = SubmitField()
-
-class CategoryForm(FlaskForm):
-    """
-    Class to create a wtf form for creating a pitch
-    """
-    name =  StringField('Category Name', validators=[Required()])
-    submit = SubmitField('Add')
+    description = TextAreaField('Add comment',validators=[Required()])
+    submit = SubmitField()
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
