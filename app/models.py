b6b12ad065e0b485f1a1041b017e64540efca7f9
sir-meee <sammymbevi@gmail.com>
2018-11-18 19:14:27 +0300

    add email to user class

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-class User(db.Model):
+from flask_login import UserMixin
+class User(UserMixin,db.Model):
+
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
-    def __repr__(self):
-        return f'User {self.username}'
+    email = db.Column(db.String(255),unique = True,index = True)
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -15,3 +16,5 @@ class User(db.Model):
         self.pass_secure = generate_password_hash(password)
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)     
+    def __repr__(self):
+        return f'User {self.username}'    
