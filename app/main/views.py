c9a66e1747aaae016775682243c804a2d7810902
sir-meee <sammymbevi@gmail.com>
2018-11-18 20:39:45 +0300

    Adds update html

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
-from flask import render_template,abort,redirect,url_for
+from flask import render_template,abort,redirect,url_for,request
 from . import main
 from flask_login import login_required
 from ..models import User
 from .forms import PitchForm, CommentForm, UpvoteForm, DownvoteForm, UpdateProfile
-from .. import db
+from .. import db, photos
 
 @main.route('/')
 def index():
@@ -61,3 +61,15 @@ def update_profile(uname):
         db.session.commit()
         return redirect(url_for('.profile',uname=user.username))
     return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))    
--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sammy:@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
 class ProdConfig(Config):
     pass
 
